e1ce3f17772d605dc0e0f1b2667704a628923d80
SuikaSibyl <570558266@qq.com>
2021-06-09 20:03:34 +0800

    update

--- pages/community/community.js
+++ pages/community/community.js
@@ -1,5 +1,12 @@
 Page({
   data: {
+    background: ['blue', 'red', 'yellow'],
+    indicatorDots: true,
+    autoplay: false,
+    vertical: false,
+    interval: 1000,
+    circular: false,
+
     orderList: [
       {
         time: '2019.4.11 12:09',
@@ -52,4 +59,29 @@ Page({
     ],
   },
   onLoad() {},
+  changeIndicatorDots(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots,
+    });
+  },
+  changeVertical() {
+    this.setData({
+      vertical: !this.data.vertical,
+    });
+  },
+  changeCircular(e) {
+    this.setData({
+      circular: !this.data.circular,
+    });
+  },
+  changeAutoplay(e) {
+    this.setData({
+      autoplay: !this.data.autoplay,
+    });
+  },
+  intervalChange(e) {
+    this.setData({
+      interval: e.detail.value,
+    });
+  },
 });
