81d64c6cc38da6a3a03d28f7bef5f9ad376edc18
Cassinicc <73009963+Cassinicc@users.noreply.github.com>
2021-06-09 19:37:37 +0800

    icon修改

--- services/__mock__/category.js
+++ services/__mock__/category.js
@@ -1,16 +1,16 @@
 export const categories = {
   data: [
     {
-      name: "本周新品",
-      children: ["男装 MEN", "女装 WOMEN", "童装 KIDS", "婴幼儿装 BABY"],
+      name: "按种类筛选",
+      children: ["果茶", "奶盖茶", "冰沙", "酸奶"],
     },
     {
       name: "本周新品",
-      children: ["男装 MEN", "女装 WOMEN", "童装 KIDS", "婴幼儿装 BABY"],
+      children: ["果茶", "奶盖茶", "冰沙", "酸奶"],
     },
     {
-      name: "本周新品",
-      children: ["男装 MEN", "女装 WOMEN", "童装 KIDS", "婴幼儿装 BABY"],
+      name: "本月新品",
+      children: ["果茶", "奶盖茶", "冰沙", "酸奶"],
     },
   ],
 };
