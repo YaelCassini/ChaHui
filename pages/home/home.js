c985b3f3f56b430c90aa985e2f1ddfa8dfb112bb
Cassinicc <73009963+Cassinicc@users.noreply.github.com>
2021-06-09 18:37:22 +0800

    首页

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,55 +1,80 @@
+import { getCommodity } from '../../services/commodity';
+import * as log from '../../utils/log';
+import { ALL, TREND, NEW, PRICE } from '../../asserts/CommodityType';
+
 Page({
   data: {
-    orderList: [
+    ALL,
+    TREND,
+    NEW,
+    PRICE,
+    searchValue: '',
+    tabs: [
       {
-        time: '2019.4.11 12:09',
-        state: '商家备货中',
-        sum: '1096.00',
-        total: 2,
-        commodity: [
-          {
-            title: '屈臣氏 碧柔致臻颜面膜64片 补水 水润 白皙 保湿 透滑',
-            description: '150ml',
-            price: '299.00',
-            num: '1',
-            image:
-              'https://gw.alipayobjects.com/mdn/rms_107da2/afts/img/A*DenfRpLr6NEAAAAAAAAAAABkARQnAQ',
-          },
-          {
-            title: '屈臣氏 碧柔致臻颜面膜64片 补水 水润 白皙 保湿 透滑',
-            description: '150ml',
-            price: '299.00',
-            num: '1',
-            image:
-              'https://gw.alipayobjects.com/mdn/rms_107da2/afts/img/A*6HXWRKGCK-oAAAAAAAAAAABkARQnAQ',
-          },
-        ],
+        id: ALL,
+        title: '综合',
       },
       {
-        time: '2019.4.11 12:09',
-        state: '配送中',
-        sum: '1096.00',
-        total: 2,
-        commodity: [
-          {
-            title: '屈臣氏 碧柔致臻颜面膜64片 补水 水润 白皙 保湿 透滑',
-            description: '150ml',
-            price: '299.00',
-            num: '1',
-            image:
-              'https://gw.alipayobjects.com/mdn/rms_107da2/afts/img/A*DenfRpLr6NEAAAAAAAAAAABkARQnAQ',
-          },
-          {
-            title: '屈臣氏 碧柔致臻颜面膜64片 补水 水润 白皙 保湿 透滑',
-            description: '150ml',
-            price: '299.00',
-            num: '1',
-            image:
-              'https://gw.alipayobjects.com/mdn/rms_107da2/afts/img/A*6HXWRKGCK-oAAAAAAAAAAABkARQnAQ',
-          },
-        ],
+        id: TREND,
+        title: '人气榜',
+      },
+      {
+        id: NEW,
+        title: '新品',
+      },
+      {
+        id: PRICE,
+        title: '价格',
+        sortable: true,
       },
     ],
+    activeTabId: ALL,
+    currentCommodities: [],
+    selectedCommodityId: '',
+    showCommodityDrawer: false,
   },
-  onLoad() {},
+  onShow() {
+    const { searchValue = '' } = getApp();
+    this.setData({ searchValue });
+    this.fetchCurrentCommodities(this.data.activeTabId);
+  },
+  onActiveTabChange(id) {
+    this.setData({ activeTabId: id });
+    this.fetchCurrentCommodities(id);
+  },
+  onTapCommodity(id) {
+    this.setData({ selectedCommodityId: id, showCommodityDrawer: true });
+  },
+  onCloseCommodityDrawer() {
+    this.setData({ showCommodityDrawer: false });
+  },
+  getCommodityDetailPagePath(id) {
+    return `/pages/commodity/commodity?id=${id}`;
+  },
+  mapCommodityItemToViewList(commodities = []) {
+    return commodities.map(item => ({
+      ...item,
+      url: this.getCommodityDetailPagePath(item.id),
+    }));
+  },
+  fetchCurrentCommodities(commodityType) {
+    this.setData({ currentCommodities: [] });
+    getCommodity({ type: commodityType })
+      .then(({ data = [] }) =>
+        this.setData({
+          currentCommodities: this.mapCommodityItemToViewList(data),
+        })
+      )
+      .catch(err =>
+        log.error('handbag.fetchCurrentCommodities.getAllCommodity', err)
+      );
+  },
+  onConfirm() {
+    this.onCloseCommodityDrawer();
+    my.showToast({
+      type: 'success',
+      content: '添加成功，在购物车等亲',
+      duration: 3000,
+    });
+  },
 });
