5fe814a751e87594db212e32c95b57eee34fd156
SuikaSibyl <60009633+SuikaSibyl@users.noreply.github.com>
2021-06-11 16:34:36 +0800

    update

--- pages/order/order.js
+++ pages/order/order.js
@@ -2,58 +2,7 @@ Page({
   data: {
     commentContent: '',
     commentScore: 4,
-    orderList: [
-      {
-        time: '2021.4.11 12:09',
-        state: '商家备货中',
-        sum: '48.00',
-        total: 2,
-        value: '内容',
-        controlled: true,
-        commodity: [
-          {
-            title: '杨枝甘露',
-            description: '少冰 三分糖',
-            price: '19.00',
-            num: '1',
-            image:
-              '../../asserts/picture/yangzhiganlu.jpg',
-          },
-          {
-            title: '草莓小确幸',
-            description: '正常冰 五分糖',
-            price: '29.00',
-            num: '1',
-            image:
-              '../../asserts/picture/caomeixiaoquexing.jpg',
-          },
-        ],
-      },
-      {
-        time: '2021.4.11 12:06',
-        state: '配送中',
-        sum: '48.00',
-        total: 2,
-        commodity: [
-          {
-            title: '杨枝甘露',
-            description: '少冰 三分糖',
-            price: '19.00',
-            num: '1',
-            image:
-              '../../asserts/picture/yangzhiganlu.jpg',
-          },
-          {
-            title: '草莓小确幸',
-            description: '正常冰 五分糖',
-            price: '29.00',
-            num: '1',
-            image:
-              '../../asserts/picture/caomeixiaoquexing.jpg',
-          },
-        ],
-      },
-    ],
+    orderList: [ ],
   },
   onLoad() {
       // Set data from db
